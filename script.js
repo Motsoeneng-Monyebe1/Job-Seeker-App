b64c30f47103ee2636a579acc95a4d795c339618
Motsoeneng-Monyebe1 <monyebe@gmail.com>
2026-08-25 22:35:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+const jobs=[
 {
-  title: "Administrative Clerk (WARMS)",
-  company: "Dept of Water & Sanitation",
-  location: "Bloemfontein",
-  salary: "R237 453 per year",
-  type: "Full-time",
-  closing: "28 Aug 2026",
-  link: "https://www.dws.gov.za/vacancies/",
-  ref: "VOCMA 44"
+title: "Administrative Clerk (WARMS)",
+company: "Dept of Water & Sanitation",
+location: "Bloemfontein",
+salary: "R237 453 per year",
+type: "Full-time",
+closing: "28 Aug 2026",
+link: "https://www.dws.gov.za/vacancies/",
+ref: "VOCMA 44"
 },
+{
+title: "Cashier",
+company: "Shoprite",
+location: "Durban",
+salary: "R4500 per month",
+type: "Full-time",
+closing: "30 Aug 2026",
+link: "https://www.shoprite.co.za",
+ref: "Shoprite-01"
+},
+{
+title: "General Worker",
+company: "Boxer",
+location: "Umlazi",
+salary: "R4000 per month",
+type: "Full-time",
+closing: "25 Aug 2026",
+link: "https://www.boxer.co.za",
+ref: "Boxer-02"
+}
+];
