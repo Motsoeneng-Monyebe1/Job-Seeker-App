bc7cfbad42957e5afcb8648cd16908e108b89cf1
Motsoeneng-Monyebe1 <monyebe@gmail.com>
2026-08-25 23:22:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,15 @@ container.innerHTML+=`<div style="border:1px solid #ddd;padding:15px;margin:12px
 });
 }
 showJobs();
+
+,
+{
+title: "Truck Driver Learnership 2026",
+company: "Unitrans",
+location: "KZN, Cape Town, Gauteng",
+salary: "Learnership Stipend",
+type: "Learnership",
+closing: "28 Aug 2026",
+link: "https://shandu.careertag.co.za/unitrans-truck-driver-learnership-opportunity-2026-grade-12-required/",
+ref: "Grade 12 Required"
+}
